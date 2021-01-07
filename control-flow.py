4af557511432ed3aed3915468f6d20407fc631bf
Arunvignesh22 <77108841+Arunvignesh22@users.noreply.github.com>
2021-01-07 20:07:09 +0530

    control-flow.py

---
+++ control-flow.py
@@ -0,0 +1,8 @@
+print("Welcome to the roller coaster")
+
+height = int(input("What is your height in cm?"))
+
+if height > 120:
+    print("You are allowed")
+else:
+    print("You are not allowed")
