188930229e26e1c75b037692497f977d12e8593e
Arunvignesh22 <77108841+Arunvignesh22@users.noreply.github.com>
2021-01-07 20:45:34 +0530

    odd or even

--- odd or even exercise.py
+++ odd or even exercise.py
@@ -1,8 +1,6 @@
 number = int(input("Which number do you want to check?"))
 
-output = number % 2
-
-if output == 0:
+if number % 2 == 0:
     print("This is an even number")
 else:
     print("This is a odd number")
