2c9fe82579e67d53b5539cdcc82981a8c4b3d676
Arunvignesh22 <77108841+Arunvignesh22@users.noreply.github.com>
2021-01-08 19:15:49 +0530

    Leap Year

--- Leap year Exercise.py
+++ Leap year Exercise.py
@@ -1,1 +1,11 @@
+
 year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0 and year % 100 != 0:
+    print(year, "is a leap year")
+elif year % 100 == 0:
+    print(year, "is not a leap year")
+elif year % 400 == 0:
+    print(year, "is a leap year")
+else:
+    print("This is not a Leap year")
